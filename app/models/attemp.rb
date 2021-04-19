90444040a21d9ef481559bf80bfd2b44ff844316
Javier Cuadrado <mantecajc@gmail.com>
2021-04-19 23:47:29 +0200

    Init attemp model

--- app/models/attemp.rb
+++ app/models/attemp.rb
@@ -2,9 +2,22 @@ class Attemp < ApplicationRecord
   # associations
   belongs_to :user
   belongs_to :quiz
+  has_many :attemp_answers
+  has_many :answers, through: :attemp_answers
 
   # validations
   validates :sucess_rate, numericality: { less_than_or_equal_to: 1, greater_than_or_equal_to: 0 }
   validates :answered_questions, presence: true,
                                  numericality: { less_than_or_equal_to: 3, greater_than_or_equal_to: 0 }
+
+  # private
+  # def up
+  #   self.answered_questions += 1
+  # end
+  # Maybe we need to create a column true_answers & false_answers
+  # if answer true
+  # true_answers += 1
+  # else
+  # false_answer += 1
+  # update sucess_rate true_answers / 3
 end
